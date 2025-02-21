83f4917c9a6dd244b1594e5f924a330e1b9e74e3
nihalzlxr <nihalroshankp9@gmail.com>
2025-02-21 16:54:26 +0530

    updated

--- lms/src/Components/Chapters.jsx
+++ lms/src/Components/Chapters.jsx
@@ -26,9 +26,10 @@ function Chapters() {
     <div className="min-h-screen bg-gray-900 bg-cover bg-center p-4 ">
       <h1 className="text-center text-white text-2xl font-bold pb-8">CHAPTERS</h1>
       {Chapters.map((chap) => (
-        <div className="w-full p-4 mb-4 rounded-lg bg-[url(../arrow.jpg)] ">
+        <div className="w-full p-4l ml-4 rounded-lg">
           <h3 className="text-white
-           font-bold -indent-4"><span className="text-red-700 p-2">{chap.id}</span> {chap.title}</h3>
+           font-bold -indent-4"><span className="text-red-700 ">{chap.id}</span> {chap.title}</h3>
+           <img src="../arrow.png" alt="" className="w-30 h-30" />
         </div>
       ))}
       <button className="fixed top-[90%] left-1/2 transform -translate-x-1/2 bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 shadow-lg transition-all duration-200" onClick={nextpage}>Continue</button>
--- lms/src/Components/Subjects.jsx
+++ lms/src/Components/Subjects.jsx
@@ -15,14 +15,14 @@ function Subjects() {
   }, []);
 
   return (
-    <div className="min-h-screen p-4 bg-gray-900">
+    <div className="h-screen p-4 bg-gray-900">
       <h1 className="text-3xl font-bold text-center text-white mb-8">
         SUBJECTS
       </h1>
       <div className="">
         {Subjects.map((sub) => (
           <Link key={sub.id} to={`/chapters/${sub.id}`}>
-            <div className="w-full flex items-center p-4 hover:opacity-50 transition-all duration-200 z-10 relative">
+            <div className="w-full flex items-center p-4 hover:opacity-50 transition-all duration-200  ">
               <div className="flex-shrink-0 w-12 h-12 bg-white rounded-full flex items-center justify-center mr-4 border-4 border-gray-800">
                 <span className="text-gray-900 font-bold">{sub.id}</span>
               </div>
