b8340fae16887bc73ab64f56d270f2835665cf0d
nihalzlxr <nihalroshankp9@gmail.com>
2025-02-21 16:56:39 +0530

    update

--- lms/src/Components/Chapters.jsx
+++ lms/src/Components/Chapters.jsx
@@ -26,10 +26,13 @@ function Chapters() {
     <div className="min-h-screen bg-gray-900 bg-cover bg-center p-4 ">
       <h1 className="text-center text-white text-2xl font-bold pb-8">CHAPTERS</h1>
       {Chapters.map((chap) => (
-        <div className="w-full p-4l ml-4 rounded-lg">
-          <h3 className="text-white
+        <div className="w-full rounded-lg">
+          <h3 className="text-white text-center
            font-bold -indent-4"><span className="text-red-700 ">{chap.id}</span> {chap.title}</h3>
+           <div className="flex justify-center align-center" >
            <img src="../arrow.png" alt="" className="w-30 h-30" />
+           </div>
+          
         </div>
       ))}
       <button className="fixed top-[90%] left-1/2 transform -translate-x-1/2 bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 shadow-lg transition-all duration-200" onClick={nextpage}>Continue</button>
